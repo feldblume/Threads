e1c54cf91e90d8e1ebfccc4d954e8b83b02b8d03
XanJe <xan7xan@gmail.com>
2014-10-30 00:40:19 +0200

    'gepp'

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,69 +7,105 @@ import java.util.Random;
 
 interface Const {
     int SIZE = 20;
+    int ITERATIONS = 20;
 }
 
 public class Main {
     public static void main(String[] args) {
-        Writer w = new Writer();
-        Sorter s = new Sorter();
-        s.run();
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter("mess.ascii"));
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Holder h = new Holder();
+
+        Writer w = new Writer(h);
         w.run();
-        Rewriter rw = new Rewriter(s);
+        Sorter s = new Sorter(h);
+        s.run();
+        Rewriter rw = new Rewriter(h);
         rw.run();
     }
 }
 
 class Holder {
     char[] chars = new char[Const.SIZE];
+    private int state;
+    Holder(){
+        state = 3;
+    }
+    public synchronized void  setState(int newState) {
+        // 1 - filled
+        // 2 - sorted
+        // 3 - ready
+        state = newState;
+        notifyAll();
+    }
+    public int getState() {
+        return state;
+    }
 }
 
 class Writer implements Runnable {
-    Holder holder;
+    final Holder holder;
     Writer(Holder h){
         holder = h;
     }
     @Override
     public void run() {
         try {
-            int i = 20;
-            while(i-- >0){
-                BufferedWriter out = new BufferedWriter(new FileWriter("mess.ascii"));
+            synchronized (holder){
+                do wait(); while(holder.getState() != 3);
+            }
+            int i = 0;
+            while(i++ < Const.ITERATIONS){
+                BufferedWriter out = new BufferedWriter(new FileWriter("mess.ascii",true));
                 Random rand = new Random();
                 out.write("<Writer start>");
-                int i = Const.SIZE;
-                while(i > 0) {
-                    holder.chars[--i] = (char)(rand.nextInt(75) + '0');
+                int j = Const.SIZE;
+                while(j > 0) {
+                    holder.chars[--j] = (char)(rand.nextInt(75) + '0');
                 }
                 out.write(holder.chars);
                 out.write("<Writer end>");
                 out.flush();
                 out.close();
+                holder.setState(1);
             }
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 }
 class Sorter implements Runnable {
-    Holder holder;
+    final Holder holder;
     Sorter(Holder h){
         holder = h;
     }
     @Override
     public  void run() {
         int i = 0;
-        while(i++ <20) {
+        while(i++ < Const.ITERATIONS) {
             try {
+                synchronized (holder){
+                    do wait(); while(holder.getState() != 1);
+                }
                 BufferedWriter out = new BufferedWriter(new FileWriter("mess.ascii", true));
                 out.write("<Sorter start>");
                 sort();
                 out.write("<Sorter end>");
                 out.flush();
                 out.close();
+                holder.setState(2);
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
@@ -87,30 +123,32 @@ class Sorter implements Runnable {
 }
 
 class Rewriter implements Runnable {
-    Holder holder;
+    final Holder holder;
     Rewriter(Holder h){
         holder = h;
     }
-    Rewriter(Sorter s) {
-        sorter = s;
-        this.chars = s.chars.clone();
-    }
+
     @Override
-    public synchronized void run() {
+    public void run() {
         try {
-//            this.wait();
-            BufferedWriter out = new BufferedWriter(new FileWriter("mess.ascii",true));
-            out.write("_Rewriter start_");
-            out.write(this.chars);
-            out.write("_Rewriter end_");
-            out.flush();
-            out.close();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
+            synchronized (holder){
+                do wait(); while(holder.getState() != 2);
+            };
+            int i = 0;
+            while(i++ < Const.ITERATIONS) {
+                BufferedWriter out = new BufferedWriter(new FileWriter("mess.ascii", true));
+                out.write("<Rewriter start>");
+                out.write(holder.chars);
+                out.write("<Rewriter end>");
+                out.flush();
+                out.close();
+                holder.setState(3);
+            }
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-
     }
 }
 
